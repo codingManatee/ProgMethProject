5652ccb94474e4f5c2b381b448eac81ba63b2869
WorkingDada <6430222521@student.chula.ac.th>
2022-12-11 19:55:27 +0700

    Check Collision

--- Game/src/logic/CollisionChecker.java
+++ Game/src/logic/CollisionChecker.java
@@ -11,9 +11,5 @@ public class CollisionChecker {
 	}
 	
 	public void checkTile(Entity entity) {
-		int entityLeftWorldX = entity.worldX + (int)(entity.solidArea.getX());
-		int entityRightWorldX = entity.worldX + (int)(entity.solidArea.getX()) + (int)(entity.solidArea.getWidth());
-		int entityTopWorldY =  entity.worldY + (int)(entity.solidArea.getY()) + (int)(entity.solidArea.getWidth());
-		
 	}
 }
